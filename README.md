30b5f744c75614f2e6cb2ab28b7723f36c3f3591
maxlovic <maxlovic@gmail.com>
2018-07-16 16:16:18 +0200

    [!] Created HTTPLiveUploadServer class

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # http-live-upload-server
+[![npm](https://img.shields.io/node/v/node-media-server.svg)](https://nodejs.org/en/)
+A simple HTTP upload and serve server to use with DASH/HLS encoder.
+
+# Todo 
+- [ ] Add command line interface
+- [ ] Add an option to specify server port
+- [ ] Add an option to specify an upload url
+- [ ] Add an option to specify the request url
+- [ ] Add CORS support
+- [ ] Add Travis support
+- [ ] Add code coverage tool
--- server.js
+++ server.js
@@ -1,50 +1,83 @@
 // Require express and create an instance of it
 var express = require('express');
 const fse = require('fs-extra')
+const extend = require( 'node.extend' );
 var HTTP_STATUS = require('http-status-codes');
-var app = express();
 
-// on the request to root (localhost:3000/)
-app.get('/', function (req, res) {
-    console.log("GET");
-    res.send('<b>My</b> first express http server');
-});
 
-// On localhost:3000/welcome
-app.get('/welcome', function (req, res) {
-    res.send('<b>Hello</b> welcome to my http server made with express');
-});
+const defaults = {
+    upload_url: '/upload',
+    directory: './',
+    overwrite: true
+};
 
-// On localhost:3000/welcome
-app.post('/', function (req, res) {
-    console.log("POST");
-    res.send('<b>POST</b> done');
-});
+const listen_defaults = {
+    port: 3000
+};
 
-// On localhost:3000/
-app.put('/*', function (req, response) {
-    const file = './' + req.path;
-    console.log("PUT " + file);
-    // With Promises:
-    fse.outputFile(file, req.body)
-    .then(() => fse.ensureFile(file))
-    .then(() => {
-        console.log('success!')
-    })
-    .catch(err => {
-        console.error(err)
-    })
 
-    response.status(HTTP_STATUS.OK).send('OK');
-});
+class HTTPLiveUploadServer {
+    constructor(options) {
+        const self = this;
+        self.options = extend({}, defaults, options);
+    }
 
-// Change the 404 message modifing the middleware
-app.use(function(req, res, next) {
-    console.log("404" + req);
-    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-});
 
-// start the server in the port 3000 !
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000.');
-});
+    attach(app) {
+        // on the request to root (localhost:3000/)
+        app.get('/', function (req, res) {
+            console.log("GET");
+            res.send('<b>My</b> first express http server');
+        });
+
+        // On localhost:3000/welcome
+        app.get('/welcome', function (req, res) {
+            res.send('<b>Hello</b> welcome to my http server made with express');
+        });
+
+        // On localhost:3000/welcome
+        app.post('/', function (req, res) {
+            console.log("POST");
+            res.send('<b>POST</b> done');
+        });
+
+        // On localhost:3000/
+        app.put('/*', function (req, response) {
+            const file = './' + req.path;
+            console.log("PUT " + file);
+            // With Promises:
+            fse.outputFile(file, req.body)
+            .then(() => fse.ensureFile(file))
+            .then(() => {
+                console.log('success!')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+
+            response.status(HTTP_STATUS.OK).send('OK');
+        });
+
+        // Change the 404 message modifing the middleware
+        app.use(function(req, res, next) {
+            console.log("404" + req);
+            res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+        });
+
+        return app;
+    };
+
+    listen(options) {
+        const self = this;
+        const opts = extend( {}, listen_defaults, options );
+        const app = express();
+        self.attach(app).listen(opts.port);
+        return self;
+    }
+
+
+}
+
+
+const server = new HTTPLiveUploadServer().listen();
+
